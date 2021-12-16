c38bd68e6d61bed2e557953f3b0a45230364cdb5
Jayu <jayeshhande00@gmail.com>
2021-12-16 21:52:50 +0530

    css and script

---
+++ script.js
@@ -0,0 +1,5 @@
+const bill_amount = document.querySelector("#bill-amount")
+const cashGiven = document.querySelector("#cash-given")
+const button = document.querySelector("#check-button")
+
+console.log(cashGiven.value)
