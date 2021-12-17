9575c1cb302f6a1609d26fc5a930287271472290
Jayu <jayeshhande00@gmail.com>
2021-12-17 08:30:14 +0530

    script complete

--- script.js
+++ script.js
@@ -1,5 +1,50 @@
-const bill_amount = document.querySelector("#bill-amount")
-const cashGiven = document.querySelector("#cash-given")
-const button = document.querySelector("#check-button")
+const billAmount = document.querySelector("#bill-amount");
+const cashGiven = document.querySelector("#cash-given");
+const button = document.querySelector("#check-button");
+const NoOfNotes = document.querySelectorAll(".NoOfNotes");
 
-console.log(cashGiven.value)
+var message = document.querySelector("#error-message")
+
+const avilabelNotes = [2000,500,200,100,50,20,10,5,1];
+button.addEventListener("click", CheckAmount);
+
+function CheckAmount()
+{
+    hideMessage()
+
+    if(billAmount.value > 0)
+    {
+       if(cashGiven.value >= billAmount.value){
+          const AmountToBeReturn = cashGiven.value - billAmount.value;
+          CalculateChange(AmountToBeReturn);
+          
+       }
+       else{
+           errorMessage("DO You Wanna A Wash Plates")
+       }
+    } 
+    else{
+      errorMessage("Bill amount should be greater than zero")
+    }  
+}
+function hideMessage(){
+    message.style.display = "none";
+
+}
+function errorMessage(msg){
+    message.style.display="block";
+    message.innerText = msg;
+
+}
+function CalculateChange(AmountToBeReturn){
+    hideMessage()
+    for(let i = 0; i < avilabelNotes.length; i++){
+        const Notes = Math.trunc( AmountToBeReturn / avilabelNotes[i]);
+
+        AmountToBeReturn = AmountToBeReturn % avilabelNotes[i];
+        NoOfNotes[i].innerText = Notes;
+      
+    };
+   
+  
+}
